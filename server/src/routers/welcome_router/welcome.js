8898abb78ab0d6ce537c18052b4f833e3f0144c6
Julia Finarovsky <juliafinarovsky@gmail.com>
2024-12-19 02:19:45 -0500

    Cleanup

--- server/src/routers/welcome_router/welcome.js
+++ server/src/routers/welcome_router/welcome.js
@@ -4,9 +4,6 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const welcome_router = express.Router();
 const path = require('path');
-const express_jwt = require('express-jwt');
-const { SECRET } = process.env;
-const jwt = require('jsonwebtoken');
 
 
 welcome_router.use(bodyParser.json());
